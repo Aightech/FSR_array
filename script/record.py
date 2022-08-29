b05100e42e748d111362bec2c9ed5f0bea04113f
aightech <long.devillard@gmail.com>
2022-08-29 15:42:44 +0100

    fixed closing issue of the serial port in the record.py script

--- script/record.py
+++ script/record.py
@@ -14,34 +14,34 @@ if len(sys.argv) < 2:
     exit()
 
 #connect to the arduino
-arduino = serial.Serial(sys.argv[1], 500000, timeout=3)
-time.sleep(1)
-
-d=b'\x00'
-print("Press 'a' to quit ")
-
-n=32
-arr = np.zeros(n*n)
-data = np.array(arr)
-time = np.array([0, 0])
-dt = [0, 0]
-while(True):
-    if keyboard.is_pressed('a'):
-        break
-    arduino.write(bytes([n]))
-    #Send position to the arduino and print in the terminal
-    prev_dt=dt[0]
-    for i in range(2):
-        d = arduino.read(4)
-        dt[i] = int.from_bytes(d, "little")
-    #print(str(dt[0]-prev_dt)+ " " + str(dt[1]-dt[0]))
+with serial.Serial(sys.argv[1], 500000, timeout=3) as arduino:
+    time.sleep(1)
     
-    for i in range(n*n):
-        d = arduino.read(2)
-        arr[i] = int.from_bytes(d, "little")
-    data = np.vstack ((data, arr))
-    time = np.vstack((time, dt))
-    #print(d)
+    d=b'\x00'
+    print("Press 'a' to quit ")
+    
+    n=32
+    arr = np.zeros(n*n)
+    data = np.array(arr)
+    time = np.array([0, 0])
+    dt = [0, 0]
+    while(True):
+        if keyboard.is_pressed('a'):
+            break
+        arduino.write(bytes([n]))
+        #Send position to the arduino and print in the terminal
+        prev_dt=dt[0]
+        for i in range(2):
+            d = arduino.read(4)
+            dt[i] = int.from_bytes(d, "little")
+            #print(str(dt[0]-prev_dt)+ " " + str(dt[1]-dt[0]))
+            
+        for i in range(n*n):
+            d = arduino.read(2)
+            arr[i] = int.from_bytes(d, "little")
+        data = np.vstack ((data, arr))
+        time = np.vstack((time, dt))
+        #print(d)
 
 dat = {'data':data, 'timestamp':time}
 savemat('data.mat', dat)
